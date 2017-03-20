20b7992f974bbad0b39b9416fe4631daf44b4a01
billy <billy@LAPTOP-CM5NATPT.home>
2017-03-20 14:57:10 +0000

    Movement half done.

--- public_html/Sprite.js
+++ public_html/Sprite.js
@@ -1,33 +1,38 @@
 //main sprite object
 function Sprite(args){
-    
-    
+ 
+     this.container = document.createElement("div");
+     this.x=args.x;
+     this.y=args.y;
     //METHODS************************************************************
     //create image on screen looks for a contact
    this.init=function(){
     //creats a place for the image to go  
-    var container = document.createElement("div");
-    container.style.position="absolute";
-    container.style.left = args.x || new Error (1, args.name, "The x position of an image is not set");
-    container.style.top = args.y || new Error (1, args.name, "The y position of the image is not set");
+   
+    this.container.style.position="absolute";
+    this.container.style.left = this.x || new Error (1, args.name, "The x position of an image is not set");
+    this.container.style.top = this.y || new Error (1, args.name, "The y position of the image is not set");
      // creats the image object
     var img = document.createElement("img");
-    img.src = args.img;
+    img.src = args.img || new Error (1, args.name, "image is not set");
     img.width = args.width || new Error (1, args.name, "Width of image is not set");
     img.height = args.height || new Error(1, args.name, "height of image was not set");
     
     //puts the image in thre space 
-    container.appendChild(img);
+    this.container.appendChild(img);
   
-   
+    
 
     // This next line will just add it to the <body> tag
-    document.body.appendChild(container);  
+    document.body.appendChild(this.container);  
    } 
    
    //moves sprite on screen
-   this.movement=function(){
-       
+   this.movement=function(xps, yps){
+       this.x=this.x + xps;
+       this.y=this.y + yps;
+       this.container.style.top=this.y + "px";
+       this.container.style.left=this.x + "px";
    }
    
    //kills objects
--- public_html/controller.js
+++ public_html/controller.js
@@ -1,5 +1,6 @@
 
-
+//*******************************************************Globals************************************************************
+var catchingBowl;
 function start(){
     var args={
         name:"bowl",
@@ -9,7 +10,14 @@ function start(){
         x:window.innerWidth*0.1,
         y:window.innerHeight*0.5,
     };
-    var bowl=new Bowl(args);
+     catchingBowl=new Bowl(args);
     
-    bowl.init();
+    catchingBowl.init();
+}
+function keyPress(event){
+    //console.log(event.keyCode);
+    
+    if(event.keyCode==97){
+            catchingBowl.movement(-100,0);
+    }    
 }
